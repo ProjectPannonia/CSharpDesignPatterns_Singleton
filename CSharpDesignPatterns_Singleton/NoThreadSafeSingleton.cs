d633157205bb5919ef46573cd6828006c74f075b
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-07-30 19:43:03 +0200

    Non thread safe singleton class implemented

---
+++ CSharpDesignPatterns_Singleton/NoThreadSafeSingleton.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharpDesignPatterns_Singleton
+{
+    // Non thead safe singleton example
+    public sealed class NoThreadSafeSingleton
+    {
+
+        private static NoThreadSafeSingleton instance = null;
+
+        // Private constructor -> Can't instantiated directly
+        private NoThreadSafeSingleton() { }
+
+        public static NoThreadSafeSingleton Instance
+        {
+            get
+            {
+                if(instance == null)
+                    instance = new NoThreadSafeSingleton();
+             
+                return instance;
+            }
+
+
+        }
+        
+
+
+
+    }
+}
